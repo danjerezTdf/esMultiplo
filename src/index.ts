15c4e31f1d84ee2462ea73b5be7ff60ae3f59332
danjerezTdf <104039810+danjerezTdf@users.noreply.github.com>
2022-05-27 15:31:52 -0300

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,13 @@
+/* Ejercicio: Múltiplos
+• Cree un método esMultiplo con 2 parámetros que devuelva el
+valor lógico verdadero o falso según si el primer número que
+se indique como parámetro es múltiplo del segundo
+• Recuerde que un numero es múltiplo de otro si al dividirlo su
+resto es cero
+• Recuerde que la operación mod permite saber si el resto de
+una división es cero
+*/
+
 let valorInUno: number = Number(prompt(`Ingrese Primer Valor:`));
 let valorInDos: number = Number(prompt(`ingrese Segundo Valor:`));
 let resultado: boolean = true;
